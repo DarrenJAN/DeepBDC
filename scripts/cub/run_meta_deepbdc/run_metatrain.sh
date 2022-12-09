ef6bd88dc206fb36060ee87c3eb78e77ee08b6a5
zz <darren782439809@gmail.com>
2022-12-09 13:41:25 -0500

    updated

--- scripts/cub/run_meta_deepbdc/run_metatrain.sh
+++ scripts/cub/run_meta_deepbdc/run_metatrain.sh
@@ -1,8 +1,8 @@
 gpuid=0
 
-DATA_ROOT=/content/CloserLookFewShot/filelists/CUB # path to the json file of CUB
-MODEL_PATH=./checkpoints/cub/ResNet18_meta_deepbdc_pretrain/last_model.tar
-cd ../../../
+DATA_ROOT=/content # path to the json file of CUB
+MODEL_PATH=/content/DeepBDC/checkpoints/cub/ResNet18_meta_deepbdc_pretrain/last_model.tar
+cd /content/DeepBDC
 
 echo "============= meta-train 1-shot ============="
 python meta_train.py --dataset cub --data_path $DATA_ROOT --model ResNet18 --method meta_deepbdc --image_size 224 --gpu ${gpuid} --lr 1e-3 --epoch 60 --milestones 40 --n_shot 1 --train_n_episode 600 --val_n_episode 600 --reduce_dim 256 --pretrain_path $MODEL_PATH
