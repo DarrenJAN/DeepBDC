8accb6fc6a2b9acfc3cafe5ef09254ba645bbed5
zz <darren782439809@gmail.com>
2022-12-09 13:51:06 -0500

    updated

--- download.sh
+++ download.sh
@@ -1,4 +1,3 @@
-git clone https://github.com/DarrenJAN/DeepBDC.git
 wget https://data.caltech.edu/records/mzrjq-6wc02/files/caltech-101.zip
 unzip /content/caltech-101.zip
 tar -zxvf /content/caltech-101/101_ObjectCategories.tar.gz
